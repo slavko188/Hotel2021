17603fecaebc5333f0885866f289d278549c9b2b
SlavkoRogi <cls>
2021-05-03 12:48:14 +0200

    Vjezbanje

--- src/services/Room/room.service.ts
+++ src/services/Room/room.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Room } from "entities/room.entity";
+import { ApiResponse } from "src/Greska/api.response.class";
 import { ApiResponseVjezbanje } from "src/Greska/Greske za vjezbanje/api.response.vjezbanje";
 import { Repository } from "typeorm";
 
@@ -8,27 +9,28 @@ import { Repository } from "typeorm";
 export class RoomService {
  
   constructor(
-     @InjectRepository(Room) 
-    private  room: Repository<Room>
-    
-  ) { }
+    @InjectRepository(Room)
+        private  room: Repository <Room>  
+   ) { }  
    
   getAll(): Promise<Room[]> {
     return this.room.find();
   }
-   
-  getById(id: number): Promise<Room | ApiResponseVjezbanje>{
+
+  getById(id: number): Promise<Room | ApiResponseVjezbanje> {
     return new Promise((resolve) => {
       this.room.findOne(id)
         .then(data => resolve(data))
         .catch(error => {
           const response: ApiResponseVjezbanje = new ApiResponseVjezbanje('error', -1002);
           resolve(response);
-        });
-    })
-    
-
-    
-
+        })
+    });
   }
-}
+          
+   
+ }
+
+  
+   
+    
